70a4da6f887cce432096662177a8d04eaa3dd373
devika-reddy251 <devikareddy4002@gmail.com>
2025-03-23 20:13:04 +0530

    a

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to the food Delivery app</h1>
+      <img src="./img.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Home
